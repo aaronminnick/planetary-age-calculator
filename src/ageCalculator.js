ffef44516f61e942ff3ddbe269a3b22834a2da9d
Aaron Minnick <abminnick@gmail.com>
2021-11-12 09:07:44 -0800

    pass age calculation test

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -5,5 +5,11 @@ export default class AgeCalculator {
     } else {
       this.age = age;
     }
+    this.mercuryAge = Math.floor(age/.24);
+    this.venusAge = Math.floor(age/.62);
+    this.marsAge = Math.floor(age/1.88);
+    this.jupiterAge = Math.floor(age/11.86);
   }
+
+
 }
