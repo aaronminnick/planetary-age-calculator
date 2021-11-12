a34845bb2d83d7c2d97b715529d1ae2724f0bb93
Aaron Minnick <abminnick@gmail.com>
2021-11-12 08:57:08 -0800

    pass test

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,6 +1,6 @@
 export default class AgeCalculator {
   constructor(age) {
-    if (typeof age !== 'number') {
+    if (typeof age !== 'number' || age !== age || age === Infinity || age === -Infinity) {
       this.age = null;
     } else {
       this.age = age;
