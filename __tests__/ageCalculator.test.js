26fad19e39e954dc820b8e6c6bea4e09c2952b6d
Aaron Minnick <abminnick@gmail.com>
2021-11-12 08:43:04 -0800

    add test for non-number input

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -4,8 +4,15 @@ import AgeCalculator from "../src/ageCalculator";
 
 describe('AgeCalculator', () => {
 
-  test('should accept an integer age in constructor and set to property this.age', () => {
+  test('Should accept an integer age in constructor and set to property this.age.', () => {
     const calc = new AgeCalculator(34);
     expect(calc.age).toEqual(34);
   });
+
+  test('Should set this.age to null when given other than a number as input.', () => {
+    const stringcalc = new AgeCalculator('thirty four');
+    expect(calc.age).toEqual(null);
+  });
+
+  
 });
