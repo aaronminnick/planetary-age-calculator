ab031cb23076b6c5034b370dbea17118079051db
Aaron Minnick <abminnick@gmail.com>
2021-11-12 08:35:24 -0800

    pass first test

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -5,7 +5,7 @@ import AgeCalculator from "../src/ageCalculator";
 describe('AgeCalculator', () => {
 
   test('should accept an integer age in constructor and set to property this.age', () => {
-    calc = new AgeCalculator(34);
+    const calc = new AgeCalculator(34);
     expect(calc.age).toEqual(34);
   });
 });
--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,5 +1,5 @@
 export default class AgeCalculator {
-  constructor() {
-
+  constructor(age) {
+    this.age = age;
   }
 }
